a7c0ea81999f70cf0b1c3f3b7f8dbc8f3d66c875
RishanaShuhaib <rishanaom@gmail.com>
2024-01-27 21:36:25 +0530

    first commit

---
+++ Zoho_Project/Company_Staff/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from Register_Login.models import *
+
+# Create your models here.
+
+#---------------- models for zoho modules--------------------
+class Unit(models.Model):
+    unit_name=models.CharField(max_length=255)
+    company=models.ForeignKey(CompanyDetails,on_delete=models.CASCADE)
+class Items(models.Model):
+    item_type=models.CharField(max_length=255)
+    item_name=models.CharField(max_length=255)
+    unit=models.ForeignKey(Unit,on_delete=models.CASCADE)
+    hsn_code=models.IntegerField(null=True,blank=True)
+    tax_reference=models.CharField(max_length=255,null=True)
+    intrastate_tax=models.IntegerField(null=True,blank=True)
+    interstate_tax=models.IntegerField(null=True,blank=True)
+
+    selling_price=models.IntegerField(null=True,blank=True)
+    sales_account=models.CharField(max_length=255)
+    sales_description=models.CharField(max_length=255)
+
+    purchase_price=models.IntegerField(null=True,blank=True)
+    purchase_account=models.CharField(max_length=255)
+    purchase_description=models.CharField(max_length=255)
+   
+    minimum_stock_to_maintain=models.IntegerField(blank=True,null=True)  
+    activation_tag=models.CharField(max_length=255,default='active')
+    inventory_account=models.CharField(max_length=255,null=True)
+
+    date=models.DateTimeField(auto_now_add=True)                                       
+
+    opening_stock=models.IntegerField(blank=True,null=True,default=0)
+    current_stock=models.IntegerField(blank=True,null=True,default=0)
+    opening_stock_per_unit=models.IntegerField(blank=True,null=True,)
+    company=models.ForeignKey(CompanyDetails,on_delete=models.CASCADE)
+    login_details=models.ForeignKey(LoginDetails,on_delete=models.CASCADE)
+
+    type=models.CharField(max_length=255,blank=True,null=True,)
+
+    track_inventory=models.IntegerField(blank=True,null=True)
+class Godown(models.Model):
+    date = models.DateField()
+    item = models.ForeignKey(Items, on_delete=models.CASCADE)
+    HSN = models.CharField(max_length=255)
+    stock_in_hand = models.IntegerField()
+    godown_name = models.CharField(max_length=255)
+    godown_address = models.CharField(max_length=255)
+    stock_keeping = models.IntegerField()
+    distance = models.IntegerField()
+    company = models.ForeignKey(CompanyDetails, on_delete=models.CASCADE)
+    login_details = models.ForeignKey(LoginDetails, on_delete=models.CASCADE)
